952f9e7779be192f6fbbe6a367a0df8f0e298330
BharatSahlot <bharatsahlot18@gmail.com>
2022-06-29 16:11:49 +0530

    hmm

---
+++ src/lib/ServiceCard.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let map;
+    export let service = {
+        image: "https://picturepan2.github.io/spectre/img/osx-yosemite-2.jpg",
+        name: "Demo Service",
+        type: "Bathroom",
+        lat: 17.4482809,
+        long: 78.3487633
+    };
+</script>
+
+<div class="column col-10 card p-0 m-2">
+    <div class="card-image" style="height: 75px;">
+        <div class="image" style="background: url({service.image})"> </div>
+    </div>
+    <div class="card-header">
+        <button class="btn btn-primary float-right" on:click={() => map.focus(service.long, service.lat)}>
+            <i class="icon icon-location"></i>
+        </button>
+        <div class="card-title h5">{service.name}</div>
+        <div class="card-subtitle text-gray">{service.type}</div>
+    </div>
+    <div class="card-footer">
+        <button class="btn btn-primary">
+            Know More
+        </button>
+    </div>
+</div>
+
+<style>
+.image
+{
+    background-size: cover;
+    height: 100%;
+    overflow-y: hidden;
+}
+</style>
---
+++ src/lib/ServiceList.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let visible = false;
+    export let map;
+    import ServiceCard from "./ServiceCard.svelte";
+
+    export function Display(tag)
+    {
+        visible = true;
+    }
+
+    export function Hide()
+    {
+        visible = false;
+    }
+</script>
+
+<div id="services" class="columns col-oneline" style="display:{visible ? 'flex':'none'}">
+    <button id="close" class="btn btn-link" on:click={() => Hide()}>
+        <i class="icon icon-cross"></i>
+    </button>
+    <ServiceCard map={map}/>
+    <ServiceCard map={map}/>
+</div>
+
+<style>
+#services
+{
+    background: white;
+    position: absolute;
+    bottom: 0%;
+    width: 100%;
+    padding-bottom: 5%;
+    padding-left: 5%;
+    padding-top: 5%;
+    margin-left: 3px;
+
+    border-top-left-radius: 50px;
+    border-top-right-radius: 50px;
+
+    box-shadow: -2px -2px 42px 4px rgba(0,0,0,0.35);
+    -webkit-box-shadow: -2px -2px 42px 4px rgba(0,0,0,0.35);
+    -moz-box-shadow: -2px -2px 42px 4px rgba(0,0,0,0.35);
+}
+#close
+{
+    position: absolute;
+    right: 30px;
+    top: -4px;
+}
+</style>
